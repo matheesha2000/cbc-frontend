e63cb9126efe0fb3d2cf2a9ccd44c6f44e1f9244
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-01-04 11:23:00 +0530

    app.jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react';
-import './App.css';
-import ProductCard from './components/productCard';
-import userData from './components/userData';
+import { useState } from 'react'
+import './App.css'
+import ProductCard from './components/productCard'
+import UserData from './components/userData'
 import LoginPage from './pages/loginPage'
+import HomePage from './pages/homePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdminHomePage from './pages/adminHomePage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+
+
 
   return (
     <div className=''>
      <BrowserRouter>
       <Toaster position='top-right'/>
-      <Routes path="/*"> 
+      <Routes path="/*">          
+        <Route path="/*" element={<HomePage/>}/>   
+        <Route path="/login" element={<LoginPage/>}/>
 
-        <Route path="/*" element={<HomePage/>}/>            
-         
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route path='/signup' element={<userData/>}/>
+        <Route path='/signup' element={<UserData/>}/>
 
         <Route path="/admin/*" element={<AdminHomePage/>}/>
-  
+        
       </Routes>
+
      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
