eda976c62bbe160cabcf1d1e5695315ccaacc8a7
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-01-08 07:37:03 +0530

    added colour themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary" : "#f3f3f3",
+        "secondary" : "#5e5d5d",
+        "accent" : "#cd7225",
+      }
+    },
   },
   plugins: [],
 }
