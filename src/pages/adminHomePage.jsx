92e9a8753eebc099b7ae69f71538e427c1b33918
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-01-17 10:07:32 +0530

    edit product form created

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -7,6 +7,7 @@ import EditProductForm from "./admin/editProductForm";
 export default function AdminHomePage() {
   return (
     <div className="bg-blue-200 w-full h-screen flex">
+      {/* Sidebar */}
       <div className="w-[20%] h-screen bg-blue-500 flex flex-col items-center py-4">
         <Link 
           className="flex flex-row items-center mb-4 text-white hover:text-blue-200" 
@@ -37,15 +38,16 @@ export default function AdminHomePage() {
         </Link>
       </div>
 
-      <div className="w-[80%] h-screen ">
-        <Routes path="/*">
-          <Route path="/" element={<h1>Dashboard</h1>} />
-          <Route path="/products" element={<AdminProductsPage/>} />
-          <Route path="/products/addProduct" element={<AddProductForm/>} />
-          <Route path="/products/editProduct" element={<EditProductForm/>} />
+      {/* Main Content */}
+      <div className="w-[80%] h-screen">
+        <Routes>
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/products" element={<AdminProductsPage />} />
+          <Route path="/products/addProduct" element={<AddProductForm />} />
+          <Route path="/products/editProduct" element={<EditProductForm />} />
           <Route path="/orders" element={<h1>Orders</h1>} />
           <Route path="/customers" element={<h1>Customers</h1>} />
-          <Route path="/*" element={<h1>404 not found the admin page</h1>}/>
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
         </Routes>
       </div>
     </div>
