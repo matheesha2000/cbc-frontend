b0849fbb74f88c5f3b446d6815e559ccca731b1e
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-01-19 12:29:19 +0530

    errors fixing

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,22 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from '../components/header';
 import LoginPage from './loginPage';
-import ProductPage from './home/product';
-import Cart from './home/cart';
+
+
+
 export default function HomePage() {
   return (
     <div className="h-screen w-full">
       <Header />
-      <div className='w-full h-[calc(100vh-100px)] '>
-        <Routes path="/*">
+      <div className="w-full h-[calc(100vh-100px)]">
+        <Routes>
           <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/products" element={<ProductPage/>} />
-          <Route path="/login" element={<LoginPage/>} />
-
-          <Route path="/cart" element={<Cart/>}/>
-        </Routes>  
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </div>
-        
     </div>
-  ); 
+  );
 }
+
