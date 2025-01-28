05a2775c4caf270c36d75226799fef2d49d32cc4
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-01-28 13:33:48 +0530

    installed google auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/homePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdminHomePage from './pages/adminHomePage'
 import { Toaster } from 'react-hot-toast'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 
 
@@ -17,11 +18,13 @@ function App() {
     <div className=''>
      <BrowserRouter>
       <Toaster position='top-right'/>
+      <GoogleOAuthProvider clientId='622865345487-ac3769lvbcj6klsvq6l2a3lo61mb289h.apps.googleusercontent.com '>
       <Routes path="/*">          
         <Route path="/*" element={<HomePage/>}/>   
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/admin/*" element={<AdminHomePage/>}/>       
       </Routes>
+      </GoogleOAuthProvider>
      </BrowserRouter>
     </div>
   )
