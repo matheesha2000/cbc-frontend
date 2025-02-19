5d3c84ef1bc3bb4de4cfbd75af55de494badd847
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-02-19 10:53:27 +0530

    added response test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from './pages/homePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AdminHomePage from './pages/adminHomePage'
 import { Toaster } from 'react-hot-toast'
-//import ResponseTest from './pages/responseTest'
+import ResponseTest from './pages/admin/responseTest'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 
@@ -20,11 +20,11 @@ function App() {
      <BrowserRouter>
       <Toaster position='top-right'/>
       <GoogleOAuthProvider clientId='622865345487-ac3769lvbcj6klsvq6l2a3lo61mb289h.apps.googleusercontent.com '>
-      <Routes path="/*">          
-        <Route path="/*" element={<HomePage/>}/>   
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/admin/*" element={<AdminHomePage/>}/>  
-            
+      <Routes path="/*">  
+         <Route path="/*" element={<HomePage/>}/>          
+        <Route path="/login" element={<LoginPage/>}/> 
+        <Route path="/admin/*" element={<AdminHomePage/>}/>   
+        <Route path="/response" element={<ResponseTest/>}/>     
       </Routes>
       </GoogleOAuthProvider>
      </BrowserRouter>
